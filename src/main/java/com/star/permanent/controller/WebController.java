fce7236a317a62a75ddfca8a24918eb93949a594
star <permanent_star@sina.com>
2015-01-12 18:15:52 +0800

    test for ResponseEntity.header & $resource=>future restangular

---
+++ src/main/java/com/star/permanent/controller/WebController.java
@@ -0,0 +1,35 @@
+package com.star.permanent.controller;
+
+import com.star.permanent.model.UserInfo;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by suheng.cloud on 2015/1/12.
+ */
+@RestController
+@RequestMapping("/rest")
+public class WebController {
+    @RequestMapping("/user")
+    public ResponseEntity<List<UserInfo>> getUserList(@RequestParam Integer page,
+                                                      @RequestParam(required = false) Integer prev,
+                                                      @RequestParam Integer pageCount){
+        List<UserInfo> userInfoList = new ArrayList<UserInfo>();
+        userInfoList.add(new UserInfo("s1","b1",21, Arrays.asList(new String[]{"admin","user"})));
+        userInfoList.add(new UserInfo("s2","b2",22, Arrays.asList(new String[]{"user"})));
+        userInfoList.add(new UserInfo("s3","b3",23, Arrays.asList(new String[]{"admin"})));
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("total","3");
+        headers.set("page",String.valueOf(page));
+        headers.set("pageCount",String.valueOf(pageCount));
+        return new ResponseEntity<List<UserInfo>>(userInfoList,headers, HttpStatus.OK);
+    }
+}
